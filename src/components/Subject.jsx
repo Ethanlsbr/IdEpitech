6e07a4746c90ff683c3fe143817d965164bde0f5
Vivicaty <valentin.guinet@epitech.eu>
2026-07-06 13:52:23 +0200

    add: way to display subject in the project

---
+++ src/components/Subject.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import remarkGfm from "remark-gfm";
+
+export default function Subject({ subject }) {
+  return (
+    <div className="flex h-full flex-col bg-[#0d1117]">
+      <object width="100%" height="100%" data={subject} type="application/pdf">
+        {" "}
+      </object>
+    </div>
+  );
+}
