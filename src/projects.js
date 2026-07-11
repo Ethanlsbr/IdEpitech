cd61d0ddeeba8f172b5c9cf4ffdb580fc4b59ded
Vivicaty <valentin.guinet@epitech.eu>
2026-07-11 16:01:44 +0200

    fix: grammar

--- src/projects.js
+++ src/projects.js
@@ -125,7 +125,7 @@ const guidedProjects = [
       "À chaque tour, regarde les cases voisines et déplace-toi si elle vaut '-' (chemin) ou 'o' (sortie).",
       "Mémorise chaque déplacement dans une liste pour savoir par où tu es passé.",
       "Si tu es bloqué, dépile ton dernier déplacement et fais le mouvement inverse (right<->left, up<->down) pour revenir en arrière.",
-      "Lèves toi va voir un manta pour lui dire leur ton top 3 spotify, si il ne l'approuve pas tu recommences.",
+      "Lèves toi va voir un manta pour lui dire ton top 3 spotify.",
     ],
   },
   {
