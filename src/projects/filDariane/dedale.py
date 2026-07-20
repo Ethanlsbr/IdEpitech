d1e177a0f28b69c61d1d65bfb606af31befa75e9
Vivicaty <valentin.guinet@epitech.eu>
2026-07-20 16:42:36 +0200

    fix: remove comment

--- src/projects/filDariane/dedale.py
+++ src/projects/filDariane/dedale.py
@@ -114,7 +114,6 @@ xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 __MAP = None
 
-# Python obfuscation by pyobfuscator.com
 _ = lambda __: __import__("zlib").decompress(__import__("base64").b64decode(__[::-1]))
 exec(
     (_)(
